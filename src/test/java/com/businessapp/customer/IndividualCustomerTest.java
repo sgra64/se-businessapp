c851069463c7ec9f4829e8277e22b79cd2621457
Steffen <kelzenberg@users.noreply.github.com>
2017-10-20 11:36:13 +0200

    started B2 JUnit implementation

--- src/test/java/com/businessapp/customer/IndividualCustomerTest.java
+++ src/test/java/com/businessapp/customer/IndividualCustomerTest.java
@@ -1,11 +1,42 @@
 package com.businessapp.customer;
 
-public class IndividualCustomerTest.java{
-/**
- * @author
- */
-public IndividualCustomerTest.java(){
-        //
-        }
+import com.businessapp.model.IndividualCustomer;
+import org.junit.*;
 
-        }
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IndividualCustomerTest {
+
+    private IndividualCustomer indicust;
+
+    @Before
+    public void setUp() {
+        indicust = new IndividualCustomer();
+    }
+
+    @Test
+    public void testSetGetFirstName(){
+        String first = "Peter";
+        indicust.setFirstName(first);
+        //assertTrue(indicust.getFirstName().equals(first));
+        assertEquals(first, indicust.getFirstName());
+
+        indicust.setFirstName(null);
+        assertEquals(null, indicust.getFirstName());
+
+    }
+
+    @Test
+    public void testSetGetName(){
+        String last = "Peterson";
+        indicust.setName(last);
+        //assertTrue(indicust.getName().equals(last));
+        assertEquals(last, indicust.getName());
+
+        indicust.setName(null);
+        assertEquals(null, indicust.getName());
+
+    }
+
+}
